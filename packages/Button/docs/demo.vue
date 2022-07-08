fc8e18c45314c0477b493ee9db2940f777d7fab9
dengxian <dengxian@gentlehealth.cn>
2022-07-08 17:50:53 +0800

    first commit

---
+++ packages/Button/docs/demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <f-button @click="onClick(1)">第一个</f-button>
+    <f-button @click="onClick(2)">第二个</f-button>
+    <f-button @click="onClick(3)">第三个</f-button>
+  </div>
+</template>
+
+<script setup>
+const onClick = (num) => { console.log(`我是第 ${num} 个自定义按钮`) }
+</script>
---
+++ packages/Button/src/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="f-button" @click="$emit('click', $event)">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineEmits(['click'])
+</script>
+
+<style scoped lang="less">
+.f-button {
+  appearance: none;
+  padding: 5px 10px;
+  background: #2080f0;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+
+  &:active {
+    background: #5cb9f3;
+  }
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+}
+</style>
